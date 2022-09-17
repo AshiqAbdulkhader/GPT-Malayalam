99e6a615f8e56aed059372b96762ef19ceab2fa4
Muhammed Ashiq Abdul Khader <ashiqabdulkhader@gmail.com>
2022-09-17 16:08:18 +0530

    model

---
+++ Test_model.py
@@ -0,0 +1,23 @@
+# test GPT2 malayalam model
+from transformers import TFGPT2LMHeadModel, GPT2Tokenizer
+
+
+output_dir = "gpt2_malayalam"
+# load tokenizer
+tokenizer = GPT2Tokenizer.from_pretrained(output_dir)
+# load model
+model = TFGPT2LMHeadModel.from_pretrained(output_dir)
+
+text = "തമിഴ്‌നാട്ടിൽ നിന്നു സഹ്യപർവ്വതം"
+input_ids = tokenizer.encode(text, return_tensors='tf')
+
+beam_output = model.generate(
+    input_ids,
+    max_length=50,
+    num_beams=5,
+    temperature=0.7,
+    no_repeat_ngram_size=2,
+    num_return_sequences=5
+)
+
+print(tokenizer.decode(beam_output[0], skip_special_tokens=True))
